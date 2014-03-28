ab3b407f43e9e18876586b59a37a1af8fa2db2b4
cramdesign <matt@Matts-iMac.local>
2014-03-27 21:26:35 -0400

    index.php
    make index more robust

--- index.php
+++ index.php
@@ -2,24 +2,45 @@
 
 	<div class="primary">
 		
-		<!-- start The Loop -->
+		<?php /* Start the Loop */ ?>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 		<article class="post">
+		
 			<header>
-				<h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+			<?php /* if its a single page, it doesn't need a link. */
+				if ( is_singular() ) : ?>
+				
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				
+			<?php else : ?>
+			
+				<h1 class="entry-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				
+			<?php endif; ?>
+				
+				
+			<?php /* pages don't need meta data */
+				if ( !is_page() ) : ?>
+				
 				<div class="meta">
-					<p class="date"><?php the_time( get_option('date_format') ); ?></p>
-					<p class="categories"><?php the_category(', ') ?></p>
-					<p class="comments"><?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?></p>
-				</div>
+					<p class="time"><?php the_time( get_option('date_format') ); ?></p>
+					<?php if( get_comments_number() != 0 ) : ?>
+			        <p class="comments"><?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?></p>
+					<?php endif; ?>
+					<?php if( has_category() ) : ?>
+					<p class="category"><?php the_category(', ') ?></p>
+					<?php endif; ?>
+				</div>/* meta */
+				
+			<?php endif; ?>
 			</header>
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div><!-- entry-content -->
+			
+			<div class="entry-content"><?php the_content(); ?></div><!-- entry-content -->
+			
 		</article><!-- post -->
 
-		<!-- end The Loop -->
+		<?php /* end the Loop */ ?>
 		<?php endwhile; endif; ?>
 
 		<nav><?php posts_nav_link(); ?></nav>
